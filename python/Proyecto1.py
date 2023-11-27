a48bcf96c8a9f72c55c4447170253743db936d4d
Valud768 <128105647+Valud768@users.noreply.github.com>
2023-11-27 11:23:33 -0600

    Add files via upload

---
+++ python/Proyecto1.py
@@ -0,0 +1,33 @@
+from PIL import Image, ImageFilter
+import cv2
+
+#Kernel
+#tamaño = (5,5)
+#coeficientes = [0, 0, -1, 0, 0, 0, -1, -2, -1, 0, -1, -2, 16, -2, -1, 0, -1, -2, -1, 0, 0, 0, -1, 0, 0]
+#Otro kernel para probar
+tamaño = (3,3)
+coeficientes = [0, -1, 0, -1, 4, -1, 0, -1, 0]
+ 
+factor = 1
+ 
+imagen_original = Image.open('imagen.jpeg')
+img1 = cv2.imread('imagen.jpeg')
+
+#Funcion en Python que determina el Laplaciano
+imagen_procesada = imagen_original.filter(ImageFilter.Kernel(tamaño, coeficientes, factor))
+ 
+#se graba el resultado 
+imagen_procesada.save('laplace-imagen.jpeg')
+img2 = cv2.imread('laplace-imagen.jpeg') 
+
+#sumamos la imagen con los bordes obtenidos del laplaciano
+resImg = cv2.add(img1,img2)
+#resImg = cv2.subtract(img1,img2)
+#cv2.imshow('Imagen', resImg)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+cv2.imwrite('imagen-mejorada.jpeg', resImg)
+
+#se cierran ambos objetos creados de la clase Image
+imagen_original.close()
+imagen_procesada.close()
